d53bda192fa6bc5faa814e698560f4b92c79146f
Fabrizio Machado <fabrizio.machado@tup-take.com>
2016-05-24 16:00:52 -0400

    Initial scaffolding - this closes #1

---
+++ Point Buddy/Point Buddy/PRPBViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PRPBViewController.swift
+//  Point Buddy
+//
+//  Created by admin mac on 5/23/16.
+//  Copyright © 2016 tuptake. All rights reserved.
+//
+
+import UIKit
+
+class PRPBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
